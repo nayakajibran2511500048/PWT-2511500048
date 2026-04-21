13292972b0baff1186511b6f48dc0298149c17f2
Aditya <2511500055@mahasiswa.atmaluhur.ac.id>
2026-04-21 23:22:38 +0700

    MEMPERBAIKI EROR

--- page/edit_kelas.php
+++ page/edit_kelas.php
@@ -9,11 +9,9 @@
 </div>
 
     <?php
-    $kd = $_GET['kd'] ?? $_GET['id'] ?? '';
+    $kd = $_GET['kd'];
+    $edit = mysqli_fetch_array(mysqli_query($koneksi,"SELECT * FROM kelas WHERE Id_kelas='$kd'"));
 
-$edit = mysqli_fetch_array(mysqli_query($koneksi,
-"SELECT * FROM kelas WHERE Id_kelas='$kd'"
-));
     if(isset($_POST['tambah'])){
         $Id_kelas = $_POST['Id_kelas'];
         $Nm_kelas = $_POST['Nm_kelas'];
